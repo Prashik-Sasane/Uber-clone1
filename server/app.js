fb7285d18a25d646d4b820b637176522fd5959ee
Prashik-Sasane <prashiksasane85@gmail.com>
2025-08-08 21:43:57 +0530

    added port

--- server/app.js
+++ server/app.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
 app.use('/user' , userRouter)
 
-app.listen(3000 , () => {
-    console.log('server is running on the port 3000');
+const PORT = 3000 || process.env.PORT; 
+
+app.listen(PORT , () => {
+    console.log(`server is running on ${PORT}`);
 })
