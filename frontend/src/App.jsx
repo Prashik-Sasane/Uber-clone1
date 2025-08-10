be1a4ade165c9a15c8b8c76ea39a84fc3dd5be3c
Prashik-Sasane <prashiksasane85@gmail.com>
2025-08-10 15:04:51 +0530

    add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import Login from './page/Login'
+import Register from './page/register'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/page/Login.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import axios from "axios";
+import {useState , useEffect} from "react"
+import { Link } from "react-router-dom";
+import { toast } from 'react-toastify';
+import gsap from "gsap";
+const Login = () => {
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("");
+        
+        useEffect(() => {
+        gsap.from(".login-form", {
+        opacity: 1,
+        y: 70,
+        duration: 3,
+        ease: "power3.out"
+        });
+    }, []);
+
+    const hangleLogin = async(e) => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:3000/api/auth/login", {email , password});
+
+            toast.success('Login Successfully', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+
+
+        } catch(err) {
+            toast.error('Something Went wrong', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        
+        });
+        }
+    }
+    return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+       <form
+          onSubmit={hangleLogin}
+          className="login-form bg-white p-8 rounded shadow-md w-full max-w-md"
+          style={{ minHeight: "500px" }}
+       >
+          <h2 className="text-2xl font-extrabold mb-6 text-center">Login Page</h2>
+          <div className="mb-6">
+              <label htmlFor="email" className="block text-gray-700 mb-2 font-extrabold">Email:</label>
+              <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={e => setemail(e.target.value)}
+                  placeholder='Ex.John'
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+          </div>
+          <div className="mb-6">
+              <label htmlFor="password" className="block text-gray-700 mb-2 font-extrabold" >Password:</label>
+              <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={e => setpassword(e.target.value)}
+                  placeholder='Ex.john@example.com'
+                  required
+                  className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+          </div>
+          <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition-colors font-bold text-2xl"
+          >
+              Login
+          </button>
+          <p className="mt-4 text-center text-gray-600">
+              Don't have an account? <Link to="/register" className="text-blue-600 hover:underline">Register</Link>
+          </p>
+       </form>
+    </div>
+  )
+}
+
+export default Login
+
